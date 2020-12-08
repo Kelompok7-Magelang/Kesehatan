6329713957d3e1dcbd760d973f1814e880bbc0ad
Kelompok7-Magelang <75665355+Kelompok7-Magelang@users.noreply.github.com>
2020-12-08 15:22:46 +0700

    Add files via upload

---
+++ data/Acuan_1.js
@@ -0,0 +1,17 @@
+var json_Acuan_1 = {
+"type": "FeatureCollection",
+"name": "Acuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.219494067927926, -7.47825512832063 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.222627302912059, -7.446501679169833 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.21814934376745, -7.500237188905039 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.146999548469907, -7.444013924089499 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.147497099485975, -7.494266576712239 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.316166893932603, -7.445506577137699 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.319649751045063, -7.505710250081774 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.22076596317963, -7.589925736040483 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.153214278144375, -7.565800134242181 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.324506050912333, -7.571429441328451 ] } }
+]
+}
